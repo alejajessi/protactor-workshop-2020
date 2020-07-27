950a1546bdb1e4decedea50208ddbb2092b84709
alejajessi <alejandra122098@gmail.com>
2020-07-27 16:30:07 -0500

    Correction of css selector

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,7 +3,7 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/*.spec.js'],
+  specs: ['../**/*.spec.js'],
   getPageTimeout: 1000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
