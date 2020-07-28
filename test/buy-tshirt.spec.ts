3b30e33176e9a9e98d9a2b6508f3986dfd520518
alejajessi <alejandra122098@gmail.com>
2020-07-28 16:48:40 -0500

    implicit expects

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -19,31 +19,21 @@ describe('Buy a t-shirt', () => {
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTShirtMenu();
-    await(browser.sleep(3000));
     await productListPage.selectProductTShirt();
-    await(browser.sleep(3000));
     await productaddedModalPage.productAdd();
-    await(browser.sleep(3000));
     await summaryStepPage.checkSignIn();
-    await(browser.sleep(3000));
 
     await signInStepPage.writeEmail(buyTShirtData.email);
     await signInStepPage.writePassword(buyTShirtData.password);
     await signInStepPage.continueToLogin();
-    await(browser.sleep(3000));
 
     await addressStepPage.checkToShipping();
-    await(browser.sleep(3000));
 
     await shippingStepPage.confirmButton();
-    await(browser.sleep(3000));
 
     await shippingStepPage.continueToPay();
-    await(browser.sleep(3000));
     await paymentStepPage.checkOutPayment();
-    await(browser.sleep(3000));
     await bankPaymentPage.checkOuttoFinish();
-    await(browser.sleep(3000));
 
     await expect(orderSummaryPage.summaryOrder()).toBe(buyTShirtData.expectedMessage);
   });
