7389bd9d0e7d1bb31e5eb9f5a3d7112d98badd22
Jessica Alejandra Sánchez <42454185+alejajessi@users.noreply.github.com>
2020-07-30 13:22:35 -0500

    add capabilities to config chrome (#17)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -11,6 +11,13 @@ export const config: Config = {
     reporter();
     browser.manage().timeouts().implicitlyWait(3000);
   },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   }
