fd152b5cc3a2ebb7c40b2dfbee2119de185adbe7
Jessica Alejandra Sánchez <42454185+alejajessi@users.noreply.github.com>
2020-07-30 10:06:35 -0500

    Add awesome jasmine (#16)
    * Add awesome jasmine
    
    * Add awesome jasmine-Delete folder
    
    * Add awesome jasmine-Delete folder

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,9 +1,21 @@
 import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
 
 export let reporter = () => {
+
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: StacktraceOption.PRETTY
     }
   }));
+
+  const { AwesomeReport } = require('jasmine-awesome-report');
+
+  const config = {
+    fullPath: 'reports',
+    fileName: 'awesome',
+    merge: true
+  };
+
+  jasmine.getEnv().addReporter(AwesomeReport.getReport(config));
+
 };
