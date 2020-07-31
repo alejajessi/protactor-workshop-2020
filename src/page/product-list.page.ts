d644cf72459e2baf0c20c49087b2093ee7b97bc6
alejajessi <alejandra122098@gmail.com>
2020-07-30 19:25:44 -0500

    delete addProduct

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -2,18 +2,15 @@ import { $, $$, ExpectedConditions, ElementFinder, ElementArrayFinder, browser }
 
 export class ProductListPage {
   private products: ElementArrayFinder;
-  private addProduct: ElementFinder;
   private timeOut: number = 7000;
 
   constructor() {
     this.products = $$('.product-container');
-    this.addProduct = $('div[class="button-container"] > a[title="Add to cart"]');
   }
 
   public async selectProduct(productName: string): Promise<void> {
     await browser.wait(ExpectedConditions.visibilityOf(this.findByProduct(productName)),
                        this.timeOut);
-    await this.addProduct.click();
     await this.findByProduct(productName).$('a.ajax_add_to_cart_button').click();
   }
 
